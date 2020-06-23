8d902bb6175ccc77dc6111ca933b67cdebbb3666
dorealex <dorealex1@gmail.com>
2020-06-22 20:23:47 -0400

    partial fix

--- predict_dept.py
+++ predict_dept.py
@@ -2,10 +2,13 @@ from sklearn.svm import LinearSVC
 #from sklearn.externals import joblib
 import joblib
 import numpy as np
-model = joblib.load("full_model2.jl")
+model = joblib.load("full_model3.jl")
 
 def predict_dept(text):
     dept = model.predict([text])[0]
     pct = np.round(model.predict_proba([text])[0],2).max()
+    #pct="ok"
     d = {'dept':dept,'pct':pct}
     return d
+
+print(predict_dept("medical marijuana"))
